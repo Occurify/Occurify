f737aaae99c52fc2e98473f502194369f5b7bc2a
Jasper <jasper.lammers@gmail.com>
2025-03-31 08:11:55 +0200

    Refactor of reactive timeline extensions.

--- src/Occurify.Reactive/Extensions/PeriodTimelineExtensions.Observable.cs
+++ src/Occurify.Reactive/Extensions/PeriodTimelineExtensions.Observable.cs
@@ -11,33 +11,36 @@ public static partial class PeriodTimelineExtensions
 {
     /// <summary>
     /// Returns a <c>IObservable</c> that emits <c>true</c> when a period starts and <c>false</c> when it ends.
-    /// If <paramref name="includeCurrentState"/> is true, the state at the current time will be emitted immediately upon subscribing.
+    /// If <paramref name="emitStateUponSubscribe"/> is true, the state at the current time will be emitted immediately upon subscribing.
     /// </summary>
-    public static IObservable<bool> ToBooleanObservable(this IPeriodTimeline periodTimeline, IScheduler scheduler, bool includeCurrentState = true) =>
-        periodTimeline.ToSampleObservable(scheduler, includeCurrentState).Select(s => s.IsPeriod);
+    public static IObservable<bool> ToBooleanObservable(this IPeriodTimeline periodTimeline, IScheduler scheduler,
+        bool emitStateUponSubscribe = true) =>
+        periodTimeline.ToSampleObservable(scheduler, emitStateUponSubscribe).Select(s => s.IsPeriod);
 
     /// <summary>
     /// Returns a <c>IObservable</c> that emits <c>true</c> when a period starts and <c>false</c> when it ends using <paramref name="relativeTo"/> as a starting time.
-    /// If <paramref name="includeCurrentState"/> is true, the state at <paramref name="relativeTo"/> will be emitted immediately upon subscribing.
+    /// If <paramref name="emitStateUponSubscribe"/> is true, the state at <paramref name="relativeTo"/> will be emitted immediately upon subscribing.
     /// </summary>
-    public static IObservable<bool> ToBooleanObservable(this IPeriodTimeline periodTimeline, DateTime relativeTo, IScheduler scheduler, bool includeCurrentState = true) => 
-        periodTimeline.ToSampleObservable(relativeTo, scheduler, includeCurrentState).Select(s => s.IsPeriod);
+    public static IObservable<bool> ToBooleanObservable(this IPeriodTimeline periodTimeline, DateTime relativeTo,
+        IScheduler scheduler, bool emitStateUponSubscribe = true) =>
+        periodTimeline.ToSampleObservable(relativeTo, scheduler, emitStateUponSubscribe).Select(s => s.IsPeriod);
 
     /// <summary>
     /// Returns a <c>IObservable</c> that emits a <see cref="PeriodTimelineSample"/> every time a period starts or ends.
-    /// If <paramref name="includeCurrentSample"/> is true, the sample at the current time will be emitted immediately upon subscribing.
+    /// If <paramref name="emitSampleUponSubscribe"/> is true, the sample at the current time will be emitted immediately upon subscribing.
     /// </summary>
     public static IObservable<PeriodTimelineSample> ToSampleObservable(this IPeriodTimeline periodTimeline,
-        IScheduler scheduler, bool includeCurrentSample = true) => periodTimeline.ToSampleObservable(DateTime.UtcNow, scheduler, includeCurrentSample);
+        IScheduler scheduler, bool emitSampleUponSubscribe = true) =>
+        periodTimeline.ToSampleObservable(DateTime.UtcNow, scheduler, emitSampleUponSubscribe);
 
     /// <summary>
     /// Returns a <c>IObservable</c> that emits a <see cref="PeriodTimelineSample"/> every time a period starts or ends using <paramref name="relativeTo"/> as a starting time.
-    /// If <paramref name="includeCurrentSample"/> is true, the sample at <paramref name="relativeTo"/> will be emitted immediately upon subscribing.
+    /// If <paramref name="emitSampleUponSubscribe"/> is true, the sample at <paramref name="relativeTo"/> will be emitted immediately upon subscribing.
     /// </summary>
     public static IObservable<PeriodTimelineSample> ToSampleObservable(this IPeriodTimeline periodTimeline,
-        DateTime relativeTo, IScheduler scheduler, bool includeCurrentSample = true)
+        DateTime relativeTo, IScheduler scheduler, bool emitSampleUponSubscribe = true)
     {
-        if (includeCurrentSample)
+        if (emitSampleUponSubscribe)
         {
             return Observable.Defer(() =>
                 periodTimeline.ToPeriodObservableInternal(relativeTo, scheduler)
@@ -67,14 +70,17 @@ public static partial class PeriodTimelineExtensions
             {
                 return null;
             }
+
             if (dateTime1 != null && dateTime2 == null)
             {
                 return dateTime1;
             }
+
             if (dateTime1 == null && dateTime2 != null)
             {
                 return dateTime2;
             }
+
             return dateTime1 < dateTime2 ? dateTime1 : dateTime2;
         }
     }
--- src/Occurify.Reactive/Extensions/TimelineExtensions.cs
+++ src/Occurify.Reactive/Extensions/TimelineExtensions.cs
@@ -12,58 +12,63 @@ public static class TimelineExtensions
 {
     /// <summary>
     /// Returns a <see cref="IObservable{Unit}"/> that emits a <see cref="Unit"/> every time an instant occurs.
+    /// If <paramref name="emitPulseUponSubscribe"/> is true, a pulse will be emitted immediately upon subscribing.
     /// </summary>
-    public static IObservable<Unit> ToPulseObservable(this ITimeline timeline, IScheduler scheduler) =>
-        timeline.ToPulseObservable(DateTime.UtcNow, scheduler);
+    public static IObservable<Unit> ToPulseObservable(this ITimeline timeline, IScheduler scheduler,
+        bool emitPulseUponSubscribe = true) =>
+        timeline.ToPulseObservable(DateTime.UtcNow, scheduler, emitPulseUponSubscribe);
 
     /// <summary>
     /// Returns a <see cref="IObservable{Unit}"/> that emits a <see cref="Unit"/> every time an instant occurs using <paramref name="relativeTo"/> as a starting time.
+    /// If <paramref name="emitPulseUponSubscribe"/> is true, a pulse will be emitted immediately upon subscribing.
     /// </summary>
-    public static IObservable<Unit> ToPulseObservable(this ITimeline timeline, DateTime relativeTo, IScheduler scheduler) => 
-        timeline.ToInstantObservable(relativeTo, scheduler).Select(_ => Unit.Default);
+    public static IObservable<Unit> ToPulseObservable(this ITimeline timeline, DateTime relativeTo,
+        IScheduler scheduler, bool emitPulseUponSubscribe = true) =>
+        timeline.ToInstantObservable(relativeTo, scheduler, emitPulseUponSubscribe).Select(_ => Unit.Default);
 
     /// <summary>
     /// Returns a <see cref="IObservable{DateTime}"/> that emits an instant as <see cref="DateTime"/> when it occurs.
+    /// If <paramref name="emitInstantUponSubscribe"/> is true, the current time will be emitted immediately upon subscribing.
     /// </summary>
-    public static IObservable<DateTime> ToInstantObservable(this ITimeline timeline, IScheduler scheduler) => 
-        timeline.ToInstantObservable(DateTime.UtcNow, scheduler);
+    public static IObservable<DateTime> ToInstantObservable(this ITimeline timeline, IScheduler scheduler,
+        bool emitInstantUponSubscribe = true) =>
+        timeline.ToInstantObservable(DateTime.UtcNow, scheduler, emitInstantUponSubscribe);
 
     /// <summary>
     /// Returns a <see cref="IObservable{DateTime}"/> that emits an instant as <see cref="DateTime"/> when it occurs using <paramref name="relativeTo"/> as a starting time.
+    /// If <paramref name="emitInstantUponSubscribe"/> is true, the instant <paramref name="relativeTo"/> will be emitted immediately upon subscribing.
     /// </summary>
-    public static IObservable<DateTime> ToInstantObservable(this ITimeline timeline, DateTime relativeTo, IScheduler scheduler) =>
-        timeline.ToSampleObservable(relativeTo, scheduler).Select(s => s.UtcSampleInstant);
-
-    /// <summary>
-    /// Returns a <see cref="IObservable{DateTime}"/> that immediately emits <see cref="DateTime.UtcNow"/> upon subscribing and then emits an instant as <see cref="DateTime"/> when it occurs.
-    /// </summary>
-    public static IObservable<DateTime> ToInstantObservableIncludingCurrentInstant(this ITimeline timeline, IScheduler scheduler)
-    {
-        return Observable.Defer(() =>
-        {
-            var utcNow = DateTime.UtcNow;
-            return timeline.ToInstantObservable(utcNow, scheduler).Prepend(utcNow);
-        });
-    }
-
-    /// <summary>
-    /// Returns a <see cref="IObservable{DateTime}"/> that immediately emits <paramref name="relativeTo"/> upon subscribing and then emits an instant as <see cref="DateTime"/> when it occurs using <paramref name="relativeTo"/> as a starting time.
-    /// </summary>
-    public static IObservable<DateTime> ToInstantObservableIncludingCurrentInstant(this ITimeline timeline, DateTime relativeTo, IScheduler scheduler)
-    {
-        return Observable.Defer(() => timeline.ToInstantObservable(relativeTo, scheduler).Prepend(relativeTo));
-    }
+    public static IObservable<DateTime> ToInstantObservable(this ITimeline timeline, DateTime relativeTo,
+        IScheduler scheduler, bool emitInstantUponSubscribe = true) =>
+        timeline.ToSampleObservable(relativeTo, scheduler, emitInstantUponSubscribe).Select(s => s.UtcSampleInstant);
 
     /// <summary>
     /// Returns a <see cref="IObservable{TimelineSample}"/> that emits a sample every time an instant occurs.
+    /// If <paramref name="emitSampleUponSubscribe"/> is true, a sample at the current time will be emitted immediately upon subscribing.
     /// </summary>
-    public static IObservable<TimelineSample> ToSampleObservable(this ITimeline timeline, IScheduler scheduler) =>
-        timeline.ToSampleObservable(DateTime.UtcNow, scheduler);
+    public static IObservable<TimelineSample> ToSampleObservable(this ITimeline timeline, IScheduler scheduler,
+        bool emitSampleUponSubscribe = true) =>
+        timeline.ToSampleObservable(DateTime.UtcNow, scheduler, emitSampleUponSubscribe);
 
     /// <summary>
     /// Returns a <see cref="IObservable{TimelineSample}"/> that emits a sample every time an instant occurs using <paramref name="relativeTo"/> as a starting time.
+    /// If <paramref name="emitSampleUponSubscribe"/> is true, the sample at <paramref name="relativeTo"/> will be emitted immediately upon subscribing.
     /// </summary>
-    public static IObservable<TimelineSample> ToSampleObservable(this ITimeline timeline, DateTime relativeTo, IScheduler scheduler)
+    public static IObservable<TimelineSample> ToSampleObservable(this ITimeline timeline, DateTime relativeTo,
+        IScheduler scheduler, bool emitSampleUponSubscribe = true)
+    {
+        if (emitSampleUponSubscribe)
+        {
+            return Observable.Defer(() =>
+                timeline.ToSampleObservableInternal(relativeTo, scheduler)
+                    .Prepend(timeline.SampleAt(relativeTo)));
+        }
+
+        return timeline.ToSampleObservableInternal(relativeTo, scheduler);
+    }
+
+    private static IObservable<TimelineSample> ToSampleObservableInternal(this ITimeline timeline, DateTime relativeTo,
+        IScheduler scheduler)
     {
         return Observable.Generate(
             timeline.GetNextUtcInstant(relativeTo),
@@ -71,25 +76,5 @@ public static class TimelineExtensions
             sample => timeline.GetNextUtcInstant(sample!.Value),
             sample => timeline.SampleAt(sample!.Value),
             sample => sample!.Value, scheduler);
-    }
-
-    /// <summary>
-    /// Returns a <see cref="IObservable{TimelineSample}"/> that immediately emits a sample upon subscribing and then emits a sample every time an instant occurs.
-    /// </summary>
-    public static IObservable<TimelineSample> ToSampleObservableIncludingCurrentInstant(this ITimeline timeline, IScheduler scheduler)
-    {
-        return Observable.Defer(() =>
-        {
-            var utcNow = DateTime.UtcNow;
-            return timeline.ToSampleObservable(utcNow, scheduler).Prepend(timeline.SampleAt(utcNow));
-        });
-    }
-
-    /// <summary>
-    /// Returns a <see cref="IObservable{TimelineSample}"/> that immediately emits a sample upon subscribing and then emits a sample every time an instant occurs using <paramref name="relativeTo"/> as a starting time.
-    /// </summary>
-    public static IObservable<TimelineSample> ToSampleObservableIncludingCurrentInstant(this ITimeline timeline, DateTime relativeTo, IScheduler scheduler)
-    {
-        return Observable.Defer(() => timeline.ToSampleObservable(relativeTo, scheduler).Prepend(timeline.SampleAt(relativeTo)));
     }
 }
