7b5ab474a01ad948c39861042691980bf1741e1d
Jasper <jasper.lammers@gmail.com>
2025-05-03 15:47:59 +0200

    Changed method naming to be in line with existing .Net methods.

--- src/Occurify.TimeZones/Extensions/DateTimeExtensions.cs
+++ src/Occurify.TimeZones/Extensions/DateTimeExtensions.cs
@@ -9,19 +9,13 @@ namespace Occurify.TimeZones.Extensions
         /// <summary>
         /// Converts <paramref name="utcDateTime"/> from Coordinated Universal Time (UTC) to <see cref="TimeZoneInfo.Local"/>.
         /// </summary>
-        public static DateTime UtcToLocalTimeZone(this DateTime utcDateTime)
-            => utcDateTime.UtcToTimeZone(TimeZoneInfo.Local);
-
-        /// <summary>
-        /// Converts <paramref name="utcDateTime"/> from Coordinated Universal Time (UTC) to <see cref="TimeZoneInfo.Local"/>.
-        /// </summary>
-        public static DateTime? UtcToLocalTimeZone(this DateTime? utcDateTime)
-            => utcDateTime.UtcToTimeZone(TimeZoneInfo.Local);
+        public static DateTime? ToLocalTime(this DateTime? utcDateTime)
+            => utcDateTime.ToTimeZone(TimeZoneInfo.Local);
 
         /// <summary>
         /// Converts <paramref name="utcDateTime"/> from Coordinated Universal Time (UTC) to timezone <paramref name="timeZone"/>.
         /// </summary>
-        public static DateTime UtcToTimeZone(this DateTime utcDateTime, TimeZoneInfo timeZone)
+        public static DateTime ToTimeZone(this DateTime utcDateTime, TimeZoneInfo timeZone)
         {
             if (utcDateTime.Kind != DateTimeKind.Utc)
             {
@@ -34,31 +28,25 @@ namespace Occurify.TimeZones.Extensions
         /// <summary>
         /// Converts <paramref name="utcDateTime"/> from Coordinated Universal Time (UTC) to timezone <paramref name="timeZone"/>.
         /// </summary>
-        public static DateTime? UtcToTimeZone(this DateTime? utcDateTime, TimeZoneInfo timeZone) => 
-            utcDateTime?.TimeZoneToUtc(timeZone);
+        public static DateTime? ToTimeZone(this DateTime? utcDateTime, TimeZoneInfo timeZone) => 
+            utcDateTime?.ToUniversalTime(timeZone);
 
         /// <summary>
         /// Converts <paramref name="localDateTime"/> from <see cref="TimeZoneInfo.Local"/> to Coordinated Universal Time (UTC).
         /// </summary>
-        public static DateTime LocalTimeZoneToUtc(this DateTime localDateTime)
-            => localDateTime.TimeZoneToUtc(TimeZoneInfo.Local);
-
-        /// <summary>
-        /// Converts <paramref name="localDateTime"/> from <see cref="TimeZoneInfo.Local"/> to Coordinated Universal Time (UTC).
-        /// </summary>
-        public static DateTime? LocalTimeZoneToUtc(this DateTime? localDateTime)
-            => localDateTime.TimeZoneToUtc(TimeZoneInfo.Local);
+        public static DateTime? ToUniversalTime(this DateTime? localDateTime)
+            => localDateTime.ToUniversalTime(TimeZoneInfo.Local);
 
         /// <summary>
         /// Converts <paramref name="dateTime"/> from timezone <paramref name="sourceTimeZone"/> to Coordinated Universal Time (UTC).
         /// </summary>
-        public static DateTime TimeZoneToUtc(this DateTime dateTime, TimeZoneInfo sourceTimeZone) =>
+        public static DateTime ToUniversalTime(this DateTime dateTime, TimeZoneInfo sourceTimeZone) =>
             TimeZoneInfo.ConvertTimeToUtc(dateTime, sourceTimeZone);
 
         /// <summary>
         /// Converts <paramref name="dateTime"/> from timezone <paramref name="sourceTimeZone"/> to Coordinated Universal Time (UTC).
         /// </summary>
-        public static DateTime? TimeZoneToUtc(this DateTime? dateTime, TimeZoneInfo sourceTimeZone) =>
-            dateTime?.TimeZoneToUtc(sourceTimeZone);
+        public static DateTime? ToUniversalTime(this DateTime? dateTime, TimeZoneInfo sourceTimeZone) =>
+            dateTime?.ToUniversalTime(sourceTimeZone);
     }
 }
