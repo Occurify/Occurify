14fe762cbb1880be437c3ff8821792437131f227
Jasper <jasper.lammers@gmail.com>
2025-05-04 13:47:32 +0200

    Implemented TimeZone ToString methods for DateTime.

--- src/Occurify.TimeZones/Extensions/DateTimeExtensions.cs
+++ src/Occurify.TimeZones/Extensions/DateTimeExtensions.cs
@@ -48,5 +48,85 @@ namespace Occurify.TimeZones.Extensions
         /// </summary>
         public static DateTime? ToUniversalTime(this DateTime? dateTime, TimeZoneInfo sourceTimeZone) =>
             dateTime?.ToUniversalTime(sourceTimeZone);
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent string representation using the formatting conventions of the current culture in timezone <paramref name="timeZone"/>.
+        /// </summary>
+        public static string ToString(this DateTime dateTime, TimeZoneInfo timeZone) => dateTime.ToTimeZone(timeZone).ToString(null, null);
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent string representation using the specified culture-specific format information in timezone <paramref name="timeZone"/>.
+        /// </summary>
+        public static string ToString(this DateTime dateTime, IFormatProvider? provider, TimeZoneInfo timeZone) => dateTime.ToTimeZone(timeZone).ToString(provider);
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent string representation using the specified format and the formatting conventions of the current culture in timezone <paramref name="timeZone"/>.
+        /// </summary>
+        public static string ToString(this DateTime dateTime, string? format, TimeZoneInfo timeZone) => dateTime.ToTimeZone(timeZone).ToString(format);
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent string representation using the specified format and culture-specific format information in timezone <paramref name="timeZone"/>.
+        /// </summary>
+        public static string ToString(this DateTime dateTime, string? format, IFormatProvider? provider, TimeZoneInfo timeZone) => dateTime.ToTimeZone(timeZone).ToString(format, provider);
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent long date string representation in timezone <paramref name="timeZone"/>.
+        /// </summary>
+        public static string ToLongDateString(this DateTime dateTime, TimeZoneInfo timeZone) => dateTime.ToTimeZone(timeZone).ToLongDateString();
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent long time string representation in timezone <paramref name="timeZone"/>.
+        /// </summary>
+        public static string ToLongTimeString(this DateTime dateTime, TimeZoneInfo timeZone) => dateTime.ToTimeZone(timeZone).ToLongTimeString();
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent short date string representation in timezone <paramref name="timeZone"/>.
+        /// </summary>
+        public static string ToShortDateString(this DateTime dateTime, TimeZoneInfo timeZone) => dateTime.ToTimeZone(timeZone).ToShortDateString();
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent short time string representation in timezone <paramref name="timeZone"/>.
+        /// </summary>
+        public static string ToShortTimeString(this DateTime dateTime, TimeZoneInfo timeZone) => dateTime.ToTimeZone(timeZone).ToShortTimeString();
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent string representation using the formatting conventions of the current culture in timezone <see cref="TimeZoneInfo.Local" />.
+        /// </summary>
+        public static string ToLocalTimeZoneString(this DateTime dateTime) => dateTime.ToString(TimeZoneInfo.Local);
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent string representation using the specified culture-specific format information in timezone <see cref="TimeZoneInfo.Local" />.
+        /// </summary>
+        public static string ToLocalTimeZoneString(this DateTime dateTime, IFormatProvider? provider) => dateTime.ToString(provider, TimeZoneInfo.Local);
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent string representation using the specified format and the formatting conventions of the current culture in timezone <see cref="TimeZoneInfo.Local" />.
+        /// </summary>
+        public static string ToLocalTimeZoneString(this DateTime dateTime, string? format) => dateTime.ToString(format, TimeZoneInfo.Local);
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent string representation using the specified format and culture-specific format information in timezone <see cref="TimeZoneInfo.Local" />.
+        /// </summary>
+        public static string ToLocalTimeZoneString(this DateTime dateTime, string? format, IFormatProvider? provider) => dateTime.ToString(format, provider, TimeZoneInfo.Local);
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent long date string representation in timezone <see cref="TimeZoneInfo.Local" />.
+        /// </summary>
+        public static string ToLocalTimeZoneLongDateString(this DateTime dateTime) => dateTime.ToLongDateString(TimeZoneInfo.Local);
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent long time string representation in timezone <see cref="TimeZoneInfo.Local" />.
+        /// </summary>
+        public static string ToLocalTimeZoneLongTimeString(this DateTime dateTime) => dateTime.ToLongTimeString(TimeZoneInfo.Local);
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent short date string representation in timezone <see cref="TimeZoneInfo.Local" />.
+        /// </summary>
+        public static string ToLocalTimeZoneShortDateString(this DateTime dateTime) => dateTime.ToShortDateString(TimeZoneInfo.Local);
+
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime" /> object to its equivalent short time string representation in timezone <see cref="TimeZoneInfo.Local" />.
+        /// </summary>
+        public static string ToLocalTimeZoneShortTimeString(this DateTime dateTime) => dateTime.ToShortTimeString(TimeZoneInfo.Local);
     }
 }
