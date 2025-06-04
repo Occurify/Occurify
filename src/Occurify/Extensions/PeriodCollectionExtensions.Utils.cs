3e7c38e8d7858e42838e6c3c4bed5722614c6b01
Jasper <jasper.lammers@gmail.com>
2025-06-04 08:13:26 +0200

    Added additional versions of existing extension methods for Period.

---
+++ src/Occurify/Extensions/PeriodCollectionExtensions.Utils.cs
@@ -0,0 +1,80 @@
+﻿using Occurify.Helpers;
+
+namespace Occurify.Extensions;
+
+/// <summary>
+/// Provides extension methods for working with <see cref="IEnumerable{Period}"/>.
+/// </summary>
+public static partial class PeriodCollectionExtensions
+{
+    /// <summary>
+    /// Determines whether <paramref name="instant"/> is on any of the periods in <paramref name="periods"/>.
+    /// </summary>
+    public static bool ContainsInstant(this IEnumerable<Period> periods, DateTime instant) =>
+        periods.Any(p => p.ContainsInstant(instant));
+
+    /// <summary>
+    /// Determines whether <paramref name="instants"/> is on any of the periods in <paramref name="periods"/>.
+    /// </summary>
+    public static bool ContainsAnyInstant(this IEnumerable<Period> periods, IEnumerable<DateTime> instants) =>
+        periods.Any(p => p.ContainsAnyInstant(instants));
+
+    /// <summary>
+    /// Determines whether any instant on <paramref name="timeline"/> is on any of the periods in <paramref name="periods"/>.
+    /// </summary>
+    public static bool ContainsAnyInstant(this IEnumerable<Period> periods, ITimeline timeline) =>
+        periods.Any(p => p.ContainsAnyInstant(timeline));
+
+    /// <summary>
+    /// Determines whether a period starting at <paramref name="periodStart"/> and ending at <paramref name="periodEnd"/> is included in any of the periods in <paramref name="periods"/>.
+    /// </summary>
+    public static bool ContainsPeriod(this IEnumerable<Period> periods, DateTime? periodStart, DateTime? periodEnd, PeriodIncludeOptions periodIncludeOptions = PeriodIncludeOptions.CompleteOnly) =>
+        periods.Any(p => p.ContainsPeriod(periodStart, periodEnd, periodIncludeOptions));
+
+    /// <summary>
+    /// Determines whether <paramref name="period"/> is included in any of the periods in <paramref name="periods"/>.
+    /// </summary>
+    public static bool ContainsPeriod(this IEnumerable<Period> periods, Period period, PeriodIncludeOptions periodIncludeOptions = PeriodIncludeOptions.CompleteOnly) =>
+        periods.Any(p => p.ContainsPeriod(period, periodIncludeOptions));
+
+    /// <summary>
+    /// Determines whether <paramref name="instant"/> is excluded by all the periods in <paramref name="periods"/>.
+    /// </summary>
+    public static bool Excludes(this IEnumerable<Period> periods, DateTime instant) => periods.All(p => p.Excludes(instant));
+
+    /// <summary>
+    /// Determines whether <paramref name="period"/> is excluded by all the periods in <paramref name="period"/>.
+    /// </summary>
+    public static bool Excludes(this IEnumerable<Period> periods, Period period) =>
+        periods.All(p => p.Excludes(period));
+
+    /// <summary>
+    /// Calculates the total duration of <paramref name="periods"/>.
+    /// </summary>
+    /// <param name="periods"></param>
+    /// <param name="mergeOverlapping">When set to <c>true</c>, overlapping parts of periods will be counted only once.</param>
+    public static TimeSpan? TotalDuration(this IEnumerable<Period> periods, bool mergeOverlapping = false)
+    {
+        if (mergeOverlapping)
+        {
+            return periods.AsPeriodTimeline().Aggregate((TimeSpan?)TimeSpan.Zero, (sum, p) =>
+            {
+                if (sum == null || p.Duration == null)
+                {
+                    return null;
+                }
+
+                return sum.Value + p.Duration.Value;
+            });
+        }
+        return periods.Aggregate((TimeSpan?)TimeSpan.Zero, (sum, p) =>
+        {
+            if (sum == null || p.Duration == null)
+            {
+                return null;
+            }
+
+            return sum.Value + p.Duration.Value;
+        });
+    }
+}
--- src/Occurify/Extensions/PeriodExtensions.Transform.cs
+++ src/Occurify/Extensions/PeriodExtensions.Transform.cs
@@ -4,39 +4,34 @@ namespace Occurify.Extensions;
 public static partial class PeriodExtensions
 {
     /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> in which all periods in <paramref name="source"/> with equal end and start instants are combined into a single period.
+    /// Returns a <see cref="IPeriodTimeline"/> in which <paramref name="source"/> is cut at <paramref name="instant"/>.
     /// </summary>
-    public static IPeriodTimeline Stitch(this IEnumerable<Period> source) => source.AsPeriodTimeline().Stitch();
+    public static IPeriodTimeline Cut(this Period source, DateTime instant) => source.AsPeriodTimeline().Cut(instant);
 
     /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> in which periods in <paramref name="source"/> are cut at <paramref name="instant"/>.
+    /// Returns a <see cref="IPeriodTimeline"/> in which <paramref name="source"/> is cut at <paramref name="instants"/>.
     /// </summary>
-    public static IPeriodTimeline Cut(this IEnumerable<Period> source, DateTime instant) => source.AsPeriodTimeline().Cut(instant);
+    public static IPeriodTimeline Cut(this Period source, IEnumerable<DateTime> instants) => source.AsPeriodTimeline().Cut(instants);
 
     /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> in which periods in <paramref name="source"/> are cut at <paramref name="instants"/>.
+    /// Returns a <see cref="IPeriodTimeline"/> in which <paramref name="source"/> is cut at <paramref name="instants"/>.
     /// </summary>
-    public static IPeriodTimeline Cut(this IEnumerable<Period> source, IEnumerable<DateTime> instants) => source.AsPeriodTimeline().Cut(instants);
+    public static IPeriodTimeline Cut(this Period source, params DateTime[] instants) => source.AsPeriodTimeline().Cut(instants);
 
     /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> in which periods in <paramref name="source"/> are cut at <paramref name="instants"/>.
+    /// Returns a <see cref="IPeriodTimeline"/> in which <paramref name="source"/> is cut at <paramref name="instants"/>.
     /// </summary>
-    public static IPeriodTimeline Cut(this IEnumerable<Period> source, params DateTime[] instants) => source.AsPeriodTimeline().Cut(instants);
+    public static IPeriodTimeline Cut(this Period source, ITimeline instants) => source.AsPeriodTimeline().Cut(instants);
 
     /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> in which periods in <paramref name="source"/> are cut at <paramref name="instants"/>.
+    /// Returns a <see cref="IPeriodTimeline"/> in which <paramref name="source"/> is cut at <paramref name="instants"/>.
     /// </summary>
-    public static IPeriodTimeline Cut(this IEnumerable<Period> source, ITimeline instants) => source.AsPeriodTimeline().Cut(instants);
+    public static IPeriodTimeline Cut(this Period source, IEnumerable<ITimeline> instants) => source.AsPeriodTimeline().Cut(instants);
 
     /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> in which periods in <paramref name="source"/> are cut at <paramref name="instants"/>.
+    /// Returns a <see cref="IPeriodTimeline"/> in which <paramref name="source"/> is cut at <paramref name="instants"/>.
     /// </summary>
-    public static IPeriodTimeline Cut(this IEnumerable<Period> source, IEnumerable<ITimeline> instants) => source.AsPeriodTimeline().Cut(instants);
-
-    /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> in which periods in <paramref name="source"/> are cut at <paramref name="instants"/>.
-    /// </summary>
-    public static IPeriodTimeline Cut(this IEnumerable<Period> source, params ITimeline[] instants) => source.AsPeriodTimeline().Cut(instants);
+    public static IPeriodTimeline Cut(this Period source, params ITimeline[] instants) => source.AsPeriodTimeline().Cut(instants);
 
     /// <summary>
     /// Returns a <see cref="IPeriodTimeline"/> with the intersections of <paramref name="source"/> with <paramref name="periodToIntersect"/>.
@@ -69,44 +64,9 @@ public static partial class PeriodExtensions
     public static IPeriodTimeline IntersectPeriods(this Period source, params IPeriodTimeline[] periodsToIntersect) => source.AsPeriodTimeline().IntersectPeriods(periodsToIntersect);
 
     /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> with the intersections of all periods in <paramref name="source"/>.
-    /// </summary>
-    public static IPeriodTimeline IntersectPeriods(this IEnumerable<Period> source) => source.Select(AsPeriodTimeline).IntersectPeriods();
-
-    /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> with the intersections of <paramref name="source"/> with <paramref name="periodsToIntersect"/>.
-    /// </summary>
-    public static IPeriodTimeline IntersectPeriods(this IEnumerable<Period> source, IEnumerable<Period> periodsToIntersect) => source.AsPeriodTimeline().IntersectPeriods(periodsToIntersect);
-
-    /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> with the intersections of <paramref name="source"/> with <paramref name="periodsToIntersect"/>.
-    /// </summary>
-    public static IPeriodTimeline IntersectPeriods(this IEnumerable<Period> source, params Period[] periodsToIntersect) => source.AsPeriodTimeline().IntersectPeriods(periodsToIntersect);
-
-    /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> with the intersections of <paramref name="source"/> with <paramref name="periodsToIntersect"/>.
-    /// </summary>
-    public static IPeriodTimeline IntersectPeriods(this IEnumerable<Period> source, IPeriodTimeline periodsToIntersect) => source.AsPeriodTimeline().IntersectPeriods(periodsToIntersect);
-
-    /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> with the intersections of <paramref name="source"/> with <paramref name="periodsToIntersect"/>.
-    /// </summary>
-    public static IPeriodTimeline IntersectPeriods(this IEnumerable<Period> source, IEnumerable<IPeriodTimeline> periodsToIntersect) => source.AsPeriodTimeline().IntersectPeriods(periodsToIntersect);
-
-    /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> with the intersections of <paramref name="source"/> with <paramref name="periodsToIntersect"/>.
-    /// </summary>
-    public static IPeriodTimeline IntersectPeriods(this IEnumerable<Period> source, params IPeriodTimeline[] periodsToIntersect) => source.AsPeriodTimeline().IntersectPeriods(periodsToIntersect);
-
-    /// <summary>
     /// Returns a <see cref="IPeriodTimeline"/> that is inverted of <paramref name="source"/>.
     /// </summary>
     public static IPeriodTimeline Invert(this Period source) => source.AsPeriodTimeline().Invert();
-
-    /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> that is inverted of all periods on <paramref name="source"/>.
-    /// </summary>
-    public static IPeriodTimeline Invert(this IEnumerable<Period> source) => source.AsPeriodTimeline().Invert();
 
     /// <summary>
     /// Merges <paramref name="source"/> with <paramref name="periodToMerge"/>. Overlap is combined.
@@ -139,41 +99,6 @@ public static partial class PeriodExtensions
     public static IPeriodTimeline Merge(this Period source, params IPeriodTimeline[] periodsToMerge) => source.AsPeriodTimeline().Merge(periodsToMerge);
 
     /// <summary>
-    /// Merges all periods in <paramref name="source"/>. Overlapping periods are combined.
-    /// </summary>
-    public static IPeriodTimeline Merge(this IEnumerable<Period> source) => source.AsPeriodTimeline();
-
-    /// <summary>
-    /// Merges all periods in <paramref name="source"/> with <paramref name="periodToMerge"/>. Overlapping periods are combined.
-    /// </summary>
-    public static IPeriodTimeline Merge(this IEnumerable<Period> source, Period periodToMerge) => source.AsPeriodTimeline().Merge(periodToMerge);
-
-    /// <summary>
-    /// Merges all periods in <paramref name="source"/> with all periods in <paramref name="periodsToMerge"/>. Overlapping periods are combined.
-    /// </summary>
-    public static IPeriodTimeline Merge(this IEnumerable<Period> source, IEnumerable<Period> periodsToMerge) => source.AsPeriodTimeline().Merge(periodsToMerge);
-
-    /// <summary>
-    /// Merges all periods in <paramref name="source"/> with all periods in <paramref name="periodsToMerge"/>. Overlapping periods are combined.
-    /// </summary>
-    public static IPeriodTimeline Merge(this IEnumerable<Period> source, params Period[] periodsToMerge) => source.AsPeriodTimeline().Merge(periodsToMerge);
-
-    /// <summary>
-    /// Merges all periods in <paramref name="source"/> with <paramref name="periodsToMerge"/>. Overlapping periods are combined.
-    /// </summary>
-    public static IPeriodTimeline Merge(this IEnumerable<Period> source, IPeriodTimeline periodsToMerge) => source.AsPeriodTimeline().Merge(periodsToMerge);
-
-    /// <summary>
-    /// Merges all periods in <paramref name="source"/> with all period timelines in <paramref name="periodsToMerge"/>. Overlapping periods are combined.
-    /// </summary>
-    public static IPeriodTimeline Merge(this IEnumerable<Period> source, IEnumerable<IPeriodTimeline> periodsToMerge) => source.AsPeriodTimeline().Merge(periodsToMerge);
-
-    /// <summary>
-    /// Merges all periods in <paramref name="source"/> with all period timelines in <paramref name="periodsToMerge"/>. Overlapping periods are combined.
-    /// </summary>
-    public static IPeriodTimeline Merge(this IEnumerable<Period> source, params IPeriodTimeline[] periodsToMerge) => source.AsPeriodTimeline().Merge(periodsToMerge);
-
-    /// <summary>
     /// Returns a <see cref="IPeriodTimeline"/> in which <paramref name="subtrahend"/> is subtracted from <paramref name="source"/>.
     /// </summary>
     public static IPeriodTimeline Subtract(this Period source, Period subtrahend) => source.AsPeriodTimeline().Subtract(subtrahend);
@@ -204,31 +129,6 @@ public static partial class PeriodExtensions
     public static IPeriodTimeline Subtract(this Period source, params IPeriodTimeline[] subtrahend) => source.AsPeriodTimeline().Subtract(subtrahend);
 
     // Note: Subtract(this IEnumerable<Period> periods) is not implemented on purpose as the signature doesn't feel logical.
-
-    /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> in which all periods in <paramref name="subtrahend"/> are subtracted from <paramref name="source"/>.
-    /// </summary>
-    public static IPeriodTimeline Subtract(this IEnumerable<Period> source, IEnumerable<Period> subtrahend) => source.AsPeriodTimeline().Subtract(subtrahend);
-
-    /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> in which all periods in <paramref name="subtrahend"/> are subtracted from <paramref name="source"/>.
-    /// </summary>
-    public static IPeriodTimeline Subtract(this IEnumerable<Period> source, params Period[] subtrahend) => source.AsPeriodTimeline().Subtract(subtrahend);
-
-    /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> in which <paramref name="subtrahend"/> is subtracted from <paramref name="source"/>.
-    /// </summary>
-    public static IPeriodTimeline Subtract(this IEnumerable<Period> source, IPeriodTimeline subtrahend) => source.AsPeriodTimeline().Subtract(subtrahend);
-
-    /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> in which all period timelines in <paramref name="subtrahend"/> are subtracted from <paramref name="source"/>.
-    /// </summary>
-    public static IPeriodTimeline Subtract(this IEnumerable<Period> source, IEnumerable<IPeriodTimeline> subtrahend) => source.AsPeriodTimeline().Subtract(subtrahend);
-
-    /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> in which all period timelines in <paramref name="subtrahend"/> are subtracted from <paramref name="source"/>.
-    /// </summary>
-    public static IPeriodTimeline Subtract(this IEnumerable<Period> source, params IPeriodTimeline[] subtrahend) => source.AsPeriodTimeline().Subtract(subtrahend);
 
     /// <summary>
     /// Offsets <paramref name="period"/> with <paramref name="offset"/>. Overflow on <c>DateTime.MinValue</c> or <c>DateTime.MaxValue</c> results in <c>null</c>.
@@ -301,52 +201,8 @@ public static partial class PeriodExtensions
     public static Period OffsetDays(this Period source, double days) => source + TimeSpan.FromDays(days);
 
     /// <summary>
-    /// Offsets <paramref name="source"/> with <paramref name="offset"/>. Overflow on <c>DateTime.MinValue</c> or <c>DateTime.MaxValue</c> results in <c>null</c>.
-    /// </summary>
-    public static IEnumerable<Period> Offset(this IEnumerable<Period> source, TimeSpan offset) => source.Select(p => p.Offset(offset));
-
-    /// <summary>
-    /// Offsets <paramref name="source"/> with <paramref name="ticks"/>. Overflow on <c>DateTime.MinValue</c> or <c>DateTime.MaxValue</c> results in <c>null</c>.
-    /// </summary>
-    public static IEnumerable<Period> OffsetTicks(this IEnumerable<Period> source, long ticks) => source.Select(p => p.OffsetTicks(ticks));
-
-    /// <summary>
-    /// Offsets <paramref name="source"/> with <paramref name="microseconds"/>. Overflow on <c>DateTime.MinValue</c> or <c>DateTime.MaxValue</c> results in <c>null</c>.
-    /// </summary>
-    public static IEnumerable<Period> OffsetMicroseconds(this IEnumerable<Period> source, double microseconds) => source.Select(p => p.OffsetMicroseconds(microseconds));
-
-    /// <summary>
-    /// Offsets <paramref name="source"/> with <paramref name="milliseconds"/>. Overflow on <c>DateTime.MinValue</c> or <c>DateTime.MaxValue</c> results in <c>null</c>.
-    /// </summary>
-    public static IEnumerable<Period> OffsetMilliseconds(this IEnumerable<Period> source, double milliseconds) => source.Select(p => p.OffsetMilliseconds(milliseconds));
-
-    /// <summary>
-    /// Offsets <paramref name="source"/> with <paramref name="seconds"/>. Overflow on <c>DateTime.MinValue</c> or <c>DateTime.MaxValue</c> results in <c>null</c>.
-    /// </summary>
-    public static IEnumerable<Period> OffsetSeconds(this IEnumerable<Period> source, double seconds) => source.Select(p => p.OffsetSeconds(seconds));
-
-    /// <summary>
-    /// Offsets <paramref name="source"/> with <paramref name="minutes"/>. Overflow on <c>DateTime.MinValue</c> or <c>DateTime.MaxValue</c> results in <c>null</c>.
-    /// </summary>
-    public static IEnumerable<Period> OffsetMinutes(this IEnumerable<Period> source, double minutes) => source.Select(p => p.OffsetMinutes(minutes));
-
-    /// <summary>
-    /// Offsets <paramref name="source"/> with <paramref name="hours"/>. Overflow on <c>DateTime.MinValue</c> or <c>DateTime.MaxValue</c> results in <c>null</c>.
-    /// </summary>
-    public static IEnumerable<Period> OffsetHours(this IEnumerable<Period> source, double hours) => source.Select(p => p.OffsetHours(hours));
-
-    /// <summary>
-    /// Offsets <paramref name="source"/> with <paramref name="days"/>. Overflow on <c>DateTime.MinValue</c> or <c>DateTime.MaxValue</c> results in <c>null</c>.
-    /// </summary>
-    public static IEnumerable<Period> OffsetDays(this IEnumerable<Period> source, double days) => source.Select(p => p.OffsetDays(days));
-
-    /// <summary>
     /// Returns a <see cref="IPeriodTimeline"/> with a single period <paramref name="period"/>.
     /// </summary>
     public static IPeriodTimeline AsPeriodTimeline(this Period period) => PeriodTimeline.FromPeriod(period);
 
-    /// <summary>
-    /// Returns a <see cref="IPeriodTimeline"/> with periods <paramref name="source"/>. Overlapping periods are combined.
-    /// </summary>
-    public static IPeriodTimeline AsPeriodTimeline(this IEnumerable<Period> source) => PeriodTimeline.FromPeriods(source);
 }
