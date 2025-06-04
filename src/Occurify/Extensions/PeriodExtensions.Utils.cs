fe837d25fafaaa7f1db452248a9a720cf5cdb529
Jasper Lammers <81427514+DevJasperNL@users.noreply.github.com>
2025-06-04 08:16:05 +0200

    Added additional versions of existing extension methods for Period. (#43)
    * Added additional versions of existing extension methods for Period.
    
    * Cleanup namespace.

--- src/Occurify/Extensions/PeriodExtensions.Utils.cs
+++ src/Occurify/Extensions/PeriodExtensions.Utils.cs
@@ -15,12 +15,6 @@ public static partial class PeriodExtensions
         (period.End == null || instant < period.End);
 
     /// <summary>
-    /// Determines whether <paramref name="instant"/> is on any of the periods in <paramref name="periods"/>.
-    /// </summary>
-    public static bool ContainsInstant(this IEnumerable<Period> periods, DateTime instant) =>
-        periods.Any(p => p.ContainsInstant(instant));
-
-    /// <summary>
     /// Determines whether any of <paramref name="instants"/> is on <paramref name="period"/>.
     /// </summary>
     public static bool ContainsAnyInstant(this Period period, IEnumerable<DateTime> instants) =>
@@ -80,63 +74,16 @@ public static partial class PeriodExtensions
     }
 
     /// <summary>
-    /// Determines whether <paramref name="period"/> is included in any of the periods in <paramref name="periods"/>.
-    /// </summary>
-    public static bool ContainsPeriod(this IEnumerable<Period> periods, Period period, PeriodIncludeOptions periodIncludeOptions = PeriodIncludeOptions.CompleteOnly) =>
-        periods.Any(p => p.ContainsPeriod(period, periodIncludeOptions));
-
-    /// <summary>
     /// Determines whether <paramref name="instant"/> is not on <paramref name="period"/>.
     /// </summary>
     public static bool Excludes(this Period period, DateTime instant) => !period.ContainsInstant(instant);
-
-    /// <summary>
-    /// Determines whether <paramref name="instant"/> is excluded by all the periods in <paramref name="periods"/>.
-    /// </summary>
-    public static bool Excludes(this IEnumerable<Period> periods, DateTime instant) => periods.All(p => p.Excludes(instant));
 
     /// <summary>
     /// Determines whether <paramref name="otherPeriod"/> is excluded by <paramref name="period"/>.
     /// </summary>
     public static bool Excludes(this Period period, Period otherPeriod) => !period.ContainsPeriod(otherPeriod, PeriodIncludeOptions.PartialAllowed);
 
-    /// <summary>
-    /// Determines whether <paramref name="period"/> is excluded by all the periods in <paramref name="period"/>.
-    /// </summary>
-    public static bool Excludes(this IEnumerable<Period> periods, Period period) =>
-        periods.All(p => p.Excludes(period));
-
     internal static bool ContainsEnd(this Period period, DateTime endOfPeriod) =>
         (period.Start == null || endOfPeriod > period.Start) &&
         (period.End == null || endOfPeriod <= period.End);
-
-    /// <summary>
-    /// Calculates the total duration of <paramref name="periods"/>.
-    /// </summary>
-    /// <param name="periods"></param>
-    /// <param name="mergeOverlapping">When set to <c>true</c>, overlapping parts of periods will be counted only once.</param>
-    public static TimeSpan? TotalDuration(this IEnumerable<Period> periods, bool mergeOverlapping = false)
-    {
-        if (mergeOverlapping)
-        {
-            return periods.AsPeriodTimeline().Aggregate((TimeSpan?)TimeSpan.Zero, (sum, p) =>
-            {
-                if (sum == null || p.Duration == null)
-                {
-                    return null;
-                }
-
-                return sum.Value + p.Duration.Value;
-            });
-        }
-        return periods.Aggregate((TimeSpan?)TimeSpan.Zero, (sum, p) =>
-        {
-            if (sum == null || p.Duration == null)
-            {
-                return null;
-            }
-
-            return sum.Value + p.Duration.Value;
-        });
-    }
 }
